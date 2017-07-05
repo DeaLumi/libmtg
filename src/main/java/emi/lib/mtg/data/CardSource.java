6e336fb308e0e2242e4ad961df84efd1ec497d53
Emi <dealuminis@gmail.com>
2017-07-04 18:25:08 -0700

    Add documentation to API
    So I guess this is kinda important.
    
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/lib/mtg/data/CardSource.java
+++ src/main/java/emi/lib/mtg/data/CardSource.java
@@ -8,16 +8,27 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 /**
- * Created by Emi on 5/7/2017.
+ * A source of information on cards in Magic: the Gathering.
  */
 @Service
 @Service.Property.String(name="name")
 public interface CardSource {
+	/**
+	 * Returns a set of cards this CardSource can provide information about. Must not be null.
+	 *
+	 * Provider note: If this set is empty, you don't know of any cards. Make sure you return at least one set.
+	 *
+	 * @return A set of cards this CardSource can provide information about, or an empty set if it doesn't know of any
+	 * sets.
+	 */
 	Collection<? extends CardSet> sets();
 
+	/**
+	 * Get a card this card source knows about by UUID.
+	 *
+	 * @param id The UUID to fetch.
+	 * @return The Card associated with that UUID (i.e. get(id).id().equals(id) is true), or null if no card could be
+	 * found.
+	 */
 	Card get(UUID id);
-
-	default Collection<Card> cards() {
-		return sets().stream().flatMap(cs -> cs.cards().stream()).collect(Collectors.toSet());
-	}
 }
