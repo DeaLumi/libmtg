ac4234894732e4136d83ec8bda0dc91ab1b64204
Emi <dealuminis@gmail.com>
2017-07-12 15:44:32 -0700

    Add set of typical card types
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/lib/mtg/characteristic/CardType.java
+++ src/main/java/emi/lib/mtg/characteristic/CardType.java
@@ -1,5 +1,9 @@
 package emi.lib.mtg.characteristic;
 
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
+
 /**
  * Created by Emi on 5/6/2016.
  */
@@ -12,6 +16,11 @@ public enum CardType {
 	Planeswalker,
 	Sorcery,
 	Tribal,
+
+	/*
+	 * Types after this are not legal in usual constructed formats.
+	 */
+
 	Plane,
 	Phenomena,
 	Vanguard,
@@ -20,11 +29,24 @@ public enum CardType {
 	Phenomenon,
 
 	/*
-	 * Types after this are non-official types; they're used in 'silly' sets like Unhinged.
+	 * Types after this are weird types; they're used in 'silly' sets like Unhinged.
+	 * (Technically 'Enchant' and 'Summon' are real (but old) types, but data for
+	 * modern sets is oracle-ized, so these types now only appear in un-sets.)
 	 */
 
 	Eaturecray,
 	Enchant,
 	Summon,
 	Player;
+
+	public static final Set<CardType> CONSTRUCTED_TYPES = Collections.unmodifiableSet(EnumSet.of(
+			CardType.Artifact,
+			CardType.Creature,
+			CardType.Enchantment,
+			CardType.Instant,
+			CardType.Land,
+			CardType.Planeswalker,
+			CardType.Sorcery,
+			CardType.Tribal
+	));
 }
