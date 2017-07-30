8dd55b99f2461757ef54bc9db9946e8d024e8c07
Emi <dealuminis@gmail.com>
2017-07-30 06:59:06 -0700

    Started to re-add XlhqImageSource
    Signed-off-by: Emi <dealuminis@gmail.com>

---
+++ src/main/java/emi/lib/mtg/impl/XlhqImageSource.java
@@ -0,0 +1,49 @@
+package emi.lib.mtg.impl;
+
+import emi.lib.Service;
+import emi.lib.mtg.Card;
+import emi.lib.mtg.ImageSource;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service.Provider(ImageSource.class)
+@Service.Property.String(name="name", value="XLHQ Images Torrent")
+@Service.Property.Number(name="priority", value=0.95)
+public class XlhqImageSource implements ImageSource {
+	private static final File PARENT = new File(new File("images"), "xlhq");
+
+	private File file(Card.Printing print) {
+		String name;
+		if (print.face(Card.Face.Kind.Right) != null) {
+			name = String.format("%s - %s", print.face(Card.Face.Kind.Left).face().name(), print.face(Card.Face.Kind.Right).face().name());
+		} else if (print.face(Card.Face.Kind.Flipped) != null) {
+			name = String.format("%s_%s", print.face(Card.Face.Kind.Front).face().name(), print.face(Card.Face.Kind.Flipped).face().name());
+		} else {
+			name = print.card().name();
+		}
+
+		File file = new File(PARENT, name);
+	}
+
+	@Override
+	public InputStream open(Card card) throws IOException {
+		return null;
+	}
+
+	@Override
+	public InputStream open(Card card, Card.Face face) throws IOException {
+		return null;
+	}
+
+	@Override
+	public InputStream open(Card.Printing printing) throws IOException {
+		return null;
+	}
+
+	@Override
+	public InputStream open(Card.Printing.Face facePrint) throws IOException {
+		return null;
+	}
+}
