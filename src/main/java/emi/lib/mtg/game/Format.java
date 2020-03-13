a651c865f878826993217832251ca16e813b2b9e
Emi <dealuminis@gmail.com>
2020-03-12 22:54:53 -0700

    Add enumeration of 'official' formats.
    It's not complete, but the goal is to just have them listed here so
    client apps (e.g. my deckbuilder!) can consult a simple list.
    
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/lib/mtg/game/Format.java
+++ src/main/java/emi/lib/mtg/game/Format.java
@@ -9,6 +9,24 @@ import java.util.Set;
 @Service
 @Service.Property.String(name="name")
 public interface Format {
+	enum Official {
+		Freeform (null),
+		Vintage (emi.lib.mtg.game.impl.formats.Vintage.class),
+		Legacy (null),
+		Modern (emi.lib.mtg.game.impl.formats.Modern.class),
+		Frontier (null),
+		Standard (emi.lib.mtg.game.impl.formats.Standard.class),
+
+		Commander (emi.lib.mtg.game.impl.formats.EDH.class),
+		Brawl (null);
+
+		public final Class<? extends Format> formatClass;
+
+		Official(Class<? extends Format> formatClass) {
+			this.formatClass = formatClass;
+		}
+	}
+
 	String name();
 
 	Set<Zone> deckZones();
