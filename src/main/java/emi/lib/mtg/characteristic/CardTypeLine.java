ed3492cf8197c1977da243bc616eefdcf970efd3
Emi <dealuminis@gmail.com>
2021-11-13 20:20:31 -0800

    CardTypeLine now must return sets
    A card can't have multiple instances of the same type. ...until the next un-set, anyway...
    
    Signed-off-by: Emi <dealuminis@gmail.com>

--- src/main/java/emi/lib/mtg/characteristic/CardTypeLine.java
+++ src/main/java/emi/lib/mtg/characteristic/CardTypeLine.java
@@ -1,16 +1,17 @@
 package emi.lib.mtg.characteristic;
 
 import java.util.Collection;
+import java.util.Set;
 
 /**
  * Created by Emi on 5/6/2016.
  */
 public interface CardTypeLine {
-	Collection<Supertype> supertypes();
+	Set<Supertype> supertypes();
 
-	Collection<CardType> cardTypes();
+	Set<CardType> cardTypes();
 
-	Collection<String> subtypes();
+	Set<String> subtypes();
 
 	default boolean isPermanent() {
 		return cardTypes().stream().anyMatch(t -> t.permanent);
